d224d69124d7440b77566767cfb5e4a3a8a19ed9
ElsoMichael <elsomichael@ymail.com>
2018-07-03 14:25:37 -0400

    subnautica

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,14 +1,14 @@
-// import React from 'react';
-// import classes from './BurgerIngredient.css';
+import React from 'react';
+import classes from './BurgerIngredient.css';
 
-// const bugerIngredient = (props) => {
-//   let ingredient = null;
+const bugerIngredient = (props) => {
+  let ingredient = null;
 
-//   switch (props.type) {
-//     case ('bread-bottom'):
-//       ingredient = <div className={classes.BreadBottom}></div>
-//   }
+  switch (props.type) {
+    case ('bread-bottom'):
+      ingredient = <div className={classes.BreadBottom}></div>
+  }
 
-// };
+};
 
-// export default bugerIngredient;
+export default bugerIngredient;
